3ce5e06f348eab2dfd502d34f0c7294fc7669dbe
Thien Nguyen <nguyenhuythien9a1@gmail.com>
2026-06-17 13:39:34 +0700

    xoa header login 6

--- src/components/layout/header/nav-items.ts
+++ src/components/layout/header/nav-items.ts
@@ -1,4 +1,4 @@
-export const navItems = [
+export const navItems: NavItem[] = [
   {
     type: 'link',
     href: '/',
@@ -29,25 +29,4 @@ export const navItems = [
     label: 'Liên hệ',
     href: '/contact',
   },
-  {
-    type: 'dropdown',
-    label: 'Trang',
-    items: [
-      { href: '/signin', label: 'Đăng nhập' },
-      { href: '/signup', label: 'Đăng ký' },
-      { href: '/reset-password', label: 'Đặt lại mật khẩu' },
-      { href: '/not-found', label: 'Lỗi 404' },
-    ],
-  },
-] satisfies NavItem[];
-
-type NavItem = Record<string, string | unknown> &
-  (
-    | {
-        type: 'link';
-        href: string;
-      }
-    | {
-        type: 'dropdown';
-      }
-  );
+];
